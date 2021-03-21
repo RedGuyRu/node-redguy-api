ee8004b8828a7f41aec3ec06457b781fd9310cda
Ilya <admin@redguy.ru>
2021-03-21 15:23:55 +0300

    Code-style fixes

--- tests/tests.js
+++ tests/tests.js
@@ -39,11 +39,11 @@ Store.Json().set("test",{time:ts}).then(() => {
             console.error("store.json.get - Error! Got " + data.test);
             process.exit(-1);
         }
-    }).catch(e => {
+    }).catch((e) => {
         console.log("store.json.get - Error!");
         console.error(e);
         process.exit(-1);
-    })
+    });
 }).catch(e => {
     console.log("store.json.set - Error!");
     console.error(e);
