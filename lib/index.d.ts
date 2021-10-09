958e092eb50e6463164b9b8dccc519952130ec53
Ilya <admin@redguy.ru>
2021-10-09 11:30:24 +0300

    Remove unsupported

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,40 +1,7 @@
 export class RedGuyApi {
     constructor(token: string, options?: { v?: string });
 
-    Math(): Math;
-
     Minecraft(): Minecraft;
-}
-
-
-export class Math {
-    constructor(options: { token: string, v: string });
-
-    get(level: string, number: number): Promise<BigInteger>;
-
-    max(level: string): Promise<number>;
-}
-
-export class NumberLevels {
-    static get factorial(): string;
-
-    static get superFactorial(): string;
-
-    static get hyperFactorial(): string;
-
-    static get megaFactorial(): string;
-
-    static get gigaFactorial(): string;
-
-    static get sixFactorial(): string;
-
-    static get sevenFactorial(): string;
-
-    static get eighthFactorial(): string;
-
-    static get nineFactorial(): string;
-
-    static get tenFactorial(): string;
 }
 
 export class Minecraft {
